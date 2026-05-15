ad61899e350834d3a48e8643fe0d79cdd5598aeb
michael-messere <michael-messere@users.noreply.github.com>
2026-05-15 15:07:57 +0000

    Deploying to gh-pages from @ michael-messere/michael-messere.github.io@ab360373d74f9d6a9491112fff01f930aee8da90 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "michael-messere/michael-messere.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
