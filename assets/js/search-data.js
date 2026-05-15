df81062883cffa3d18a101758347efe9109b61f9
michael-messere <michael-messere@users.noreply.github.com>
2026-05-15 15:34:00 +0000

    Deploying to gh-pages from @ michael-messere/michael-messere.github.io@e4971bfa7f7220ffea89ef5424e921a9236cdd81 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "An overview of my research projects, past and present.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
